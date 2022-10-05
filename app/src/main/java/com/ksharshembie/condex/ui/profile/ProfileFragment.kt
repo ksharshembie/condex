ef63e2344eb3d1bc51ec58dea3e799523af7d6fe
os-ksharshembie <kadyrbek.sharshembiev@gmail.com>
2022-10-05 22:45:30 +0600

    Master Code commit + Profile Fragment added and ImagePickUp from Gallery done

---
+++ app/src/main/java/com/ksharshembie/condex/ui/profile/ProfileFragment.kt
@@ -0,0 +1,39 @@
+package com.ksharshembie.condex.ui.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import com.ksharshembie.condex.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ivProfile.setOnClickListener {
+            imagePickup.launch("image/*")
+        }
+    }
+
+    private val imagePickup = registerForActivityResult(ActivityResultContracts.GetContent(),
+        ActivityResultCallback {
+            binding.ivProfile.setImageURI(it)
+        })
+
+}
